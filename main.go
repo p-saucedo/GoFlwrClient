d6b816bdfd2a8de029499a7bb3a1841438ced8c4
pablo <pablo.saucedodm@gmail.com>
2022-12-09 01:06:43 +0100

    Gorgonia NN

--- main.go
+++ main.go
@@ -3,10 +3,35 @@ package main
 import (
 	c "goflwr/src/go/flwr/client"
 	"log"
+
+	tensor "gorgonia.org/tensor"
 )
 
+type CustomClient struct {
+	c.IClient
+}
+
+func (client *CustomClient) GetParameters(config map[string]interface{}) []*tensor.Dense {
+	return p[:]
+}
+
+func (client *CustomClient) GetProperties(config map[string]interface{}) map[string]interface{} {
+	return map[string]interface{}{"hola": 1}
+}
+
+func (client *CustomClient) Fit(parameters []*tensor.Dense, config map[string]interface{}) ([]*tensor.Dense, int, map[string]interface{}) {
+	return parameters, 12, map[string]interface{}{"test": "fit"}
+}
+
+func (client *CustomClient) Evaluate(parameters []*tensor.Dense, config map[string]interface{}) (float32, int, map[string]interface{}) {
+	return 0.12, 2, map[string]interface{}{"test": "evaluate"}
+}
+
+var p = [1]*tensor.Dense{tensor.New(tensor.WithShape(2, 2), tensor.WithBacking([]float64{1, 2, 3, 4}))}
+
 func main() {
-	client := &c.CustomClient{}
+
+	client := &CustomClient{}
 	log.Println("Starting client...")
 	err := c.StartClient("127.0.0.1:8080", client)
 	if err != nil {
